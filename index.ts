0d5983e676b5059a11112d558e37978792acab29
Nick Atkins <nbatkins@gmail.com>
2021-09-04 17:41:17 -0500

    add ts files

---
+++ index.ts
@@ -0,0 +1,13 @@
+// this is imported from ../index.ts
+// Console Output
+console.log("[example/index] Hello, World!");
+
+import './test';
+import './customcommand';
+import './custommotd';
+import './inventory';
+import './form';
+import './attribute';
+import './ipban';
+import './actor-effects';
+// import './permissions';
